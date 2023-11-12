7a9fd065b70065cdce9bb8e95a6ab7b18890610a
MichailoZaichenko <michailo.zaichenko@gmail.com>
2023-11-12 23:09:54 +0200

    add random

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import discord # Подключаем библиотеку
 from discord.ext import commands
 from tk import token
+import random
 
 intents = discord.Intents.default() # Подключаем "Разрешения"
 intents.message_content = True
@@ -8,6 +9,7 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='>', intents=intents) 
 
 hello_words = ['hello', 'hi', ' privet', 'ky', "привет", "здарова"]
+yaki_ti_Woddy = ['bot', 'lox', 'pidr', 'shluxa']
 
 @bot.event
 async def on_ready():
@@ -16,12 +18,13 @@ async def on_ready():
 # С помощью декоратора создаём первую команду
 @bot.command(pass_context = True)
 async def botic(ctx, arg):
-    if arg:
+    try:
         author = ctx.message.author
         # response_text = f'{author.mention} + arg бот!!' * 10
         # response_text = f'{author.mention}' + arg
-        response_text = f'{arg} бот!! \n'
-    else:
+        random_option = random.choice(yaki_ti_Woddy)
+        response_text = f'{arg} {random_option}!! \n'
+    except TypeError:
         response_text = 'Введи текст після команди >botic'
     for i in range(10):
         await ctx.send(response_text)
