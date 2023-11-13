b6bfc6fc3c2b9325a16f029e39b228951a9a42bd
MichailoZaichenko <michailo.zaichenko@gmail.com>
2023-11-13 21:02:23 +0200

    add more fundtions

--- main.py
+++ main.py
@@ -1,38 +1,104 @@
-import discord
+import discord # Подключаем библиотеку
 from discord.ext import commands
 from tk import token
+import random
 
-# Указываем интенты для вашего бота
-intents = discord.Intents.default()
-intents.typing = False
-intents.presences = False
-
-# Создаем экземпляр клиента бота
-bot = commands.Bot(command_prefix='.', intents=intents)
-
+intents = discord.Intents.default() # Подключаем "Разрешения"
+intents.message_content = True
+# add prefix
+PREFIX = '>'
+# Задаём префикс и интенты
+bot = commands.Bot(command_prefix= PREFIX , intents=intents) 
+bot.remove_command("help")
+# lists for commands
+hello_words = ['hello', 'hi', ' privet', 'ky', "привет", "здарова"]
+yaki_ti_Woddy = ['bot', 'lox', 'pidr', 'shluxa']
+# Announsment about connecting bot to server
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} подключился к Discord!')
-    
+    print('Bot connected')
+
+# С помощью декоратора создаём первую команду
+@bot.command(pass_context = True)
+async def botic(ctx, arg):
+    try:
+        author = ctx.message.author
+        # response_text = f'{author.mention} + arg бот!!' * 10
+        # response_text = f'{author.mention}' + arg
+        random_option = random.choice(yaki_ti_Woddy)
+        response_text = f'{arg} {random_option}!! \n'
+    except TypeError:
+        response_text = 'Введи текст після команди >botic'
+    for i in range(10):
+        await ctx.send(response_text)
+
 # @bot.event
 # async def on_message(message):
-#     print(f'Received message: {message.content}')  # Add this line for debugging
-#     if message.content.startswith('!'):
-#         text = message.content[1:]
-#         response_text = f'{text} бот ' * 10
-#         await message.channel.send(response_text)
-#     await bot.process_commands(message)
+#     msg = message.content.lower()
+#     if msg in hello_words:
+#         await message.channel.send('Hi. What do you want?')
 
-# @bot.command(pass_context = True)
-# async def on_message(message):
-#     print(f'Received message: {message.content}')
-#     response_text = f'{message.content} бот!! ' * 10
-#     await message.channel.send(response_text)
+# help command
+@bot.command(pass_context = True)
+async def help(ctx):
+    emb = discord.Embed(title = "Navigation be commands")
 
-@bot.event
-async def hello(ctx):
-    print(f'Received message: {ctx.content}')
-    await ctx.send('hello')
+    emb.add_field(name='{}hello'.format( PREFIX ), value = 'If you write greetings into chat it will answer')
+    emb.add_field(name='{}clear'.format( PREFIX ), value ="Chat clear")
+    emb.add_field(name='{}kick'.format( PREFIX ), value ="Kick from the server")
+    emb.add_field(name='{}ban'.format( PREFIX ), value = ' Ban from server')
+    emb.add_field(name='{}unban'.format( PREFIX ), value = 'Unban')
+    emb.add_field(name='{}botic'.format( PREFIX ), value = "Shits some one you write")
 
-# Токен вашего бота, который нужно получить на https://discord.com/developers/applications
+    await ctx.send( embed = emb)
+
+# Del messages
+@bot.command(pass_context = True)
+@commands.has_permissions(administrator = True)
+async def clear(ctx, amount=100):
+    await ctx.channel.purge(limit = amount)
+
+# Hello to user
+@bot.command(pass_context =True)
+async def hello(ctx, amount = 1):
+    await ctx.channel.purge(limit = amount)
+
+    author = ctx.message.author
+    await ctx.send(f'Hello {author.mention}')
+
+# Ban
+@bot.command(pass_context = True)
+@commands.has_permissions(administrator = True)
+
+async def ban(ctx, member: discord.Member, *, reason = None):
+    await ctx.channel.purge(limit = 1)
+    await member.ban(reason=reason)
+    await ctx.send(f'ban user {member.mention}')
+
+# UnBan
+@bot.command(pass_context = True)
+@commands.has_permissions(administrator = True)
+
+async def unban(ctx, *, member):
+    await ctx.channel.purge(limit = 1)
+    # banned users list
+    banned_users = await ctx.guild.bans()
+    # user from list
+    for ban_entry in banned_users:
+        user = ban_entry.user
+        # unban user
+        await ctx.guild.unban(user)
+        await ctx.send(f'unban user {member.mention}')
+        return
+
+# Kick
+@bot.command(pass_context = True)
+@commands.has_permissions(administrator = True)
+
+async def kick(ctx, member: discord.Member, *, reason = None):
+    await ctx.channel.purge(limit = 1)
+    await member.kick(reason=reason)
+    await ctx.send(f'kick user {member.mention}')
+
+
 bot.run(token)
